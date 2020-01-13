35d70b61ccf3e75c65d80ea2665930df045c32de
Thomas Jejkal <thomas.jejkal@kit.edu>
2020-01-13 09:42:44 +0100

    Update to Spring Boot 2.2.2 Required update to Springfox Swagger 3.0.0-SNAPSHOT

--- src/main/java/edu/kit/datamanager/collection/configuration/SwaggerUiConfiguration.java
+++ src/main/java/edu/kit/datamanager/collection/configuration/SwaggerUiConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.util.List;
 
@@ -22,55 +22,58 @@ import java.util.List;
 @Configuration
 @ComponentScan(basePackages = "edu.kit.datamanager")
 @EnableWebMvc
-@EnableSwagger2 //Loads the spring beans required by the framework
+@EnableSwagger2WebMvc //Loads the spring beans required by the framework
 @PropertySource("classpath:swagger.properties")
 @Import(SwaggerDocumentationConfig.class)
-public class SwaggerUiConfiguration extends WebMvcConfigurerAdapter {
-  private static final String[] SERVLET_RESOURCE_LOCATIONS = { "/" };
+public class SwaggerUiConfiguration extends WebMvcConfigurerAdapter{
+
+  private static final String[] SERVLET_RESOURCE_LOCATIONS = {"/"};
 
   private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-      "classpath:/META-INF/resources/", "classpath:/resources/",
-      "classpath:/static/", "classpath:/public/" };
+    "classpath:/META-INF/resources/", "classpath:/resources/",
+    "classpath:/static/", "classpath:/public/"};
 
   private static final String[] RESOURCE_LOCATIONS;
-  static {
+
+  static{
     RESOURCE_LOCATIONS = new String[CLASSPATH_RESOURCE_LOCATIONS.length
-        + SERVLET_RESOURCE_LOCATIONS.length];
+            + SERVLET_RESOURCE_LOCATIONS.length];
     System.arraycopy(SERVLET_RESOURCE_LOCATIONS, 0, RESOURCE_LOCATIONS, 0,
-        SERVLET_RESOURCE_LOCATIONS.length);
+            SERVLET_RESOURCE_LOCATIONS.length);
     System.arraycopy(CLASSPATH_RESOURCE_LOCATIONS, 0, RESOURCE_LOCATIONS,
-        SERVLET_RESOURCE_LOCATIONS.length, CLASSPATH_RESOURCE_LOCATIONS.length);
+            SERVLET_RESOURCE_LOCATIONS.length, CLASSPATH_RESOURCE_LOCATIONS.length);
   }
 
   private static final String[] STATIC_INDEX_HTML_RESOURCES;
-  static {
+
+  static{
     STATIC_INDEX_HTML_RESOURCES = new String[RESOURCE_LOCATIONS.length];
-    for (int i = 0; i < STATIC_INDEX_HTML_RESOURCES.length; i++) {
+    for(int i = 0; i < STATIC_INDEX_HTML_RESOURCES.length; i++){
       STATIC_INDEX_HTML_RESOURCES[i] = RESOURCE_LOCATIONS[i] + "index.html";
     }
   }
 
   @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    if (!registry.hasMappingForPattern("/webjars/**")) {
+  public void addResourceHandlers(ResourceHandlerRegistry registry){
+    if(!registry.hasMappingForPattern("/webjars/**")){
       registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-    if (!registry.hasMappingForPattern("/**")) {
+    if(!registry.hasMappingForPattern("/**")){
       registry.addResourceHandler("/**").addResourceLocations(RESOURCE_LOCATIONS);
     }
   }
 
   @Bean
-  public Jackson2ObjectMapperBuilder builder() {
+  public Jackson2ObjectMapperBuilder builder(){
     Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
-        .indentOutput(true)
-        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-        .dateFormat(new RFC3339DateFormat());
+            .indentOutput(true)
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .dateFormat(new RFC3339DateFormat());
     return builder;
   }
 
   @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+  public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
     converters.add(new MappingJackson2HttpMessageConverter(objectMapper()));
     super.configureMessageConverters(converters);
   }
