f4b8146b36500547f5f60d0c85b2594f037ee59c
ThomasJejkal <thomas.jejkal@kit.edu>
2020-08-20 12:32:19 +0200

    Updated docker setup to be based on DockerHub image

--- dockerInstall/binary/collection/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /kitdm20/collection-registry/release
-java -cp ".:./config:./collection-registry.jar" -Dloader.path=file://`pwd`/collection-registry.jar,./lib/,. -jar collection-registry.jar
