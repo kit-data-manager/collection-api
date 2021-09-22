2132d4f09982fa9228c0aa94b9ae1c2bbeb53072
Jan Schweikert <jan.schweikert@kit.edu>
2021-09-22 18:07:40 +0200

    using InputStream on resources, removed unused code

--- src/main/java/edu/kit/datamanager/collection/web/impl/CollectionApiUIImpl.java
+++ src/main/java/edu/kit/datamanager/collection/web/impl/CollectionApiUIImpl.java
@@ -24,12 +24,10 @@ import edu.kit.datamanager.collection.dto.EditorRequestCollection;
 import edu.kit.datamanager.collection.dto.EditorRequestMember;
 import edu.kit.datamanager.collection.dto.TabulatorItems;
 import edu.kit.datamanager.collection.web.CollectionApiUI;
-import edu.kit.datamanager.exceptions.CustomInternalServerError;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -55,15 +53,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class CollectionApiUIImpl implements CollectionApiUI {
-    /*
-    private final static String DATAMODELCOLLECTION = "/static/jsonSchemas/dataModelCollection.json";
-    private final static String UIFORMCOLLECTION = "/static/jsonSchemas/uiFormCollection.json";
-    private final static String ITEMSCOLLECTION = "/static/jsonSchemas/itemsCollection.json";
 
-    private final static String DATAMODELMEMBER = "/static/jsonSchemas/dataModelMember.json";
-    private final static String UIFORMMEMBER = "/static/jsonSchemas/uiFormMember.json";
-    private final static String ITEMSMEMBER = "/static/jsonSchemas/itemsMember.json";
-    */
     private final static Resource DATAMODEL_COLLECTION= new ClassPathResource("/static/jsonSchemas/dataModelCollection.json");
     private final static Resource UIFORM_COLLECTION= new ClassPathResource("/static/jsonSchemas/uiFormCollection.json");
     private final static Resource ITEMS_COLLECTION= new ClassPathResource("/static/jsonSchemas/itemsCollection.json");
@@ -91,10 +81,10 @@ public class CollectionApiUIImpl implements CollectionApiUI {
         List<CollectionObject> collections = collectionDao.findAll();
 
         EditorRequestCollection request = EditorRequestCollection.builder()
-                .dataModel(getJsonObject2(DATAMODEL_COLLECTION))
-                .uiForm(getJsonObject2(UIFORM_COLLECTION))
+                .dataModel(getJsonObject(DATAMODEL_COLLECTION))
+                .uiForm(getJsonObject(UIFORM_COLLECTION))
                 .collections(collections)
-                .items(getJsonArrayOfItems2(ITEMS_COLLECTION)).build();
+                .items(getJsonArrayOfItems(ITEMS_COLLECTION)).build();
 
         ModelAndView model = new ModelAndView("collections");
         model.addObject("request", request);
@@ -114,10 +104,10 @@ public class CollectionApiUIImpl implements CollectionApiUI {
         List<MemberItem> memberItems = memberResultSet.getBody().getContents();
 
         EditorRequestMember memberRequest = EditorRequestMember.builder()
-                .dataModel(getJsonObject2(DATAMODEL_MEMBER))
-                .uiForm(getJsonObject2(UIFORM_MEMBER))
+                .dataModel(getJsonObject(DATAMODEL_MEMBER))
+                .uiForm(getJsonObject(UIFORM_MEMBER))
                 .members(memberItems)
-                .items(getJsonArrayOfItems2(ITEMS_MEMBER))
+                .items(getJsonArrayOfItems(ITEMS_MEMBER))
                 .collectionId(id).build();
 
         ModelAndView model = new ModelAndView("members");
@@ -128,23 +118,10 @@ public class CollectionApiUIImpl implements CollectionApiUI {
     /**
      * gets a JSON object from a file.
      *
-     * @param path path of the file.
+     * @param resource resource file.
      * @return JSON object.
      */
-    private JSONObject getJsonObject(String path) {
-        Resource resource = new ClassPathResource(path);
-        JSONParser parser = new JSONParser();
-        JSONObject obj = null;
-        try {
-            obj = (JSONObject) parser.parse(
-                    new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return obj;
-    }
-
-    private JSONObject getJsonObject2(Resource resource) {
+    private JSONObject getJsonObject(Resource resource) {
         JSONParser parser = new JSONParser();
         JSONObject obj = null;
         try {
@@ -158,27 +135,14 @@ public class CollectionApiUIImpl implements CollectionApiUI {
     /**
      * gets an array of TabulatorItems from a file.
      *
-     * @param path path of a file.
+     * @param resource resource file.
      * @return array of TabulatorItems.
      */
-    private TabulatorItems[] getJsonArrayOfItems(String path) {
-        ObjectMapper mapper = new ObjectMapper();
-        TabulatorItems[] items = null;
-        Resource resource = new ClassPathResource(path);
-        try {
-            //items = mapper.readValue(Files.newBufferedReader(Paths.get(resource.getURI()), StandardCharsets.UTF_8), TabulatorItems[].class);
-            items = mapper.readValue(resource.getFile(), TabulatorItems[].class);
-        } catch (IOException ex) {
-            Logger.getLogger(CollectionApiUIImpl.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return items;
-    }
-
-    private TabulatorItems[] getJsonArrayOfItems2(Resource resource) {
+    private TabulatorItems[] getJsonArrayOfItems(Resource resource) {
         ObjectMapper mapper = new ObjectMapper();
         TabulatorItems[] items = null;
         try {
-            items = mapper.readValue(resource.getFile(), TabulatorItems[].class);
+            items = mapper.readValue(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8), TabulatorItems[].class);
         } catch (IOException ex) {
             Logger.getLogger(CollectionApiUIImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
